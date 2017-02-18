844c498e9988aaf46f6be2ff920409b4e0a5d36a
John Doherty <doherty.j@cambridgeassessment.org.uk>
2017-02-18 14:30:46 +0000

    added test framework and CI builds

---
+++ test/jsdoctojsonschema.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var toJsonSchema = require('../index.js');
+
+describe('jsdoc-to-json-schema', function () {
+
+    it('should be defined', function () {
+        expect(toJsonSchema).toBeDefined();
+    });
+});
