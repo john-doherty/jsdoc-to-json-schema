014295cb9e1bc445e0f788a7d80d7b65d7e086e3
John Doherty <doherty.j@cambridgeassessment.org.uk>
2017-02-18 13:46:06 +0000

    modified index to only require objects when needed

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var program = require('commander'),
-    pjson = require('./package.json');
- 
-if (module.parent){
+ if (module.parent){
     module.exports = require('./lib/jsdoc-to-json-schema.js');
 }
 else {
-    
-    program
-    .version(pjson.version)
-    .option('-i, --input <string>', 'path to input file or folder')
-    .option('-o, --output <string>', 'path to output file or folder')
-    .parse(process.argv);
+
+    var program = require('commander'),
+        pjson = require('./package.json');
+
+    program.version(pjson.version)
+        .option('-i, --input <string>', 'path to input file or folder')
+        .option('-o, --output <string>', 'path to output file or folder')
+        .parse(process.argv);
     
     module.exports = new require('./lib/jsdoc-to-json-schema.js')(program.input, program.output);    
 }
